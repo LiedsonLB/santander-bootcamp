41c005ec5c92cd5f6ce02f28d4d48f68c7fe16a5
liedsonlb <liedson.b9@gmail.com>
2025-06-14 20:36:42 -0300

    feat: add challenge 2

---
+++ desafio2_ControleFluxo/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package desafio2_ControleFluxo.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
